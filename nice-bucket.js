60f724e4b0522d232ca074afd824d4e0022af4d2
Ryan Flood <rflood@cmu.edu>
2014-08-31 15:50:35 -0400

    No more ice buckets
    also, this most likely works but defintely has bugs somewhere`

--- nice-bucket.js
+++ nice-bucket.js
@@ -1,2 +1,10 @@
 $(document).ready(function() {
+  $("#stream_pagelet").bind("DOMSubtreeModified", function() {
+    $('.userContentWrapper').each(function(i){ 
+      if($(this).children().text().match(/ ?ice ?-?bucket/i) != null){ 
+        $(this).parent().hide(); 
+      }  
+    });
+  });
 });
+
